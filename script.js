d65870f052783ef980240bb34e0fd733c1114d51
Aleksander vaage <vaage04@gmail.com>
2021-09-08 13:09:29 +0200

    design + rydding

--- script.js
+++ script.js
@@ -1,35 +1,60 @@
 
 var myTable = document.getElementById("data")
+let navnArr = [];
+let asArr = [];
+let webArr = [];
+let infoArr = [];
 function søk(){
-    var søkeord = String(document.getElementById("søkefelt").value)
+    var søkeord = String(document.getElementById("SearchNavn").value)
     var apiLink = String("https://data.brreg.no/enhetsregisteret/api/enheter?navn=" + søkeord)
-    console.log(apiLink)
+    navnArr = [];
+    asArr = [];
+    webArr = [];
+    infoArr = [];
+    
+    for(let i = 0; i < 6; i++){
+        var x = String("navn" + (i + 1))
+        document.getElementById(x).innerHTML = i + 1;
+    }
+
 
     $(document).ready(function() {
         $.getJSON(apiLink, function(results) {
             const json = (results._embedded.enheter)
             
             for (let i = 0; i < json.length; i++) {
-                myTable.rows[i+1].cells[0].innerHTML = json[i].navn;
-                myTable.rows[i+1].cells[1].innerHTML = json[i].organisasjonsform.beskrivelse;
-                if(json[i].hjemmeside != undefined){
-                myTable.rows[i+1].cells[2].innerHTML = json[i].hjemmeside;
-                }
-                else{
-                    myTable.rows[i+1].cells[2].innerHTML = "Nettside ikke tilgjengelig"
-                }
-                if(json[i].registrertIMvaregisteret == true && json[i].frivilligMvaRegistrertBeskrivelser != undefined){    
-                    myTable.rows[i+1].cells[3].innerHTML = json[i].frivilligMvaRegistrertBeskrivelser;
-                }
                 
+                
+                navnArr.push(json[i].navn);
+                asArr.push(json[i].organisasjonsform.beskrivelse);
+
+
+                if(json[i].hjemmeside != undefined){
+                webArr.push(json[i].hjemmeside);
+                }
                 else{
-                    myTable.rows[i+1].cells[3].innerHTML = "Beskrivelse ikke tilgjengelig"
+                    webArr.push("Nettside ikke tilgjengelig");
                 }
 
+                if(json[i].registrertIMvaregisteret == true && json[i].frivilligMvaRegistrertBeskrivelser != undefined){
+                    infoArr.push(json[i].frivilligMvaRegistrertBeskrivelser);
+
+                }
+                else{
+                    infoArr.push("Beskrivelse ikke tilgjengelig");
+                }
                 
+                
+
+                var navni = String("navn" + (i + 1))
+                var typei = String("type" + (i + 1))
+                var webi = String("web" + (i + 1))
+                document.getElementById(navni).innerHTML = navnArr[i]
+                document.getElementById(typei).innerHTML = asArr[i]
+                document.getElementById(webi).innerHTML = webArr[i]
 
 
             }
+        });
     });
-});
 }
